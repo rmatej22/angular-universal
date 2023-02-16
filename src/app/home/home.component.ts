f91d9541306798e1d89ee08addb1fe6927da5a9b
rmatej22 <rmatej545@gmail.com>
2023-02-16 20:15:59 +0100

    set title in home component for better seo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "../services/courses.service";
-import {map, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+import { CoursesService } from '../services/courses.service';
+import { map, tap } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-    courses$: Observable<Course[]>;
+  constructor(private coursesService: CoursesService, private title: Title) {}
 
-    constructor(private coursesService: CoursesService) {
+  ngOnInit() {
+    this.title.setTitle('Angular Universal Course - All Courses');
 
-    }
-
-    ngOnInit() {
-        this.courses$ = this.coursesService.findAllCourses()
-            .pipe(
-                map(Object.values)
-            );
-    }
-
+    this.courses$ = this.coursesService
+      .findAllCourses()
+      .pipe(map(Object.values));
+  }
 }
