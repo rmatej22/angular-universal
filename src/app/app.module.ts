85bbec7c371a4b380d5e461b77099c1381ff6ee3
rmatej22 <rmatej545@gmail.com>
2023-02-17 17:22:26 +0100

    create directive that detects if app is server side redered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,38 @@
-import {BrowserModule, BrowserTransferStateModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {
+  BrowserModule,
+  BrowserTransferStateModule,
+} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button'
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
-import {CourseComponent} from "./course/course.component";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatSelectModule } from "@angular/material/select";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatSortModule } from "@angular/material/sort";
-import { MatTableModule } from "@angular/material/table";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import {CoursesService} from "./services/courses.service";
-import {CourseResolver} from "./services/course.resolver";
+import { CourseComponent } from './course/course.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CoursesService } from './services/courses.service';
+import { CourseResolver } from './services/course.resolver';
 import { CourseDialogComponent } from './course-dialog/course-dialog.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { HttpClientModule} from '@angular/common/http';
-import {AboutComponent} from './about/about.component';
-
-
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './about/about.component';
+import { AppShellRenderDirective } from './directives/app-shell-render.directive';
 
 @NgModule({
   declarations: [
@@ -41,7 +41,8 @@ import {AboutComponent} from './about/about.component';
     CourseComponent,
     CoursesCardListComponent,
     CourseDialogComponent,
-    AboutComponent
+    AboutComponent,
+    AppShellRenderDirective,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
@@ -64,13 +65,9 @@ import {AboutComponent} from './about/about.component';
     MatSelectModule,
     MatDatepickerModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [
-    CoursesService,
-    CourseResolver
-  ],
-  bootstrap: [AppComponent]
+  providers: [CoursesService, CourseResolver],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
